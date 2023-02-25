b91cf7ce5063c7dfc8c49a5da1f7801c03fae2a1
Stephanie Tavares <78547969+stephanie-tavares@users.noreply.github.com>
2023-02-25 16:37:59 -0300

    Create CrashLoopBackOff.sh

---
+++ CrashLoopBackOff.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in $(kubectl get pods -A | grep crash | awk '{ print $1,$2 }' | tr -s ' ' ':')
+do
+namespace = $(echo $i | cut -f1 -d:)
+pod = $(echo $i | cut -f2 -d:)
+kubectl delete pods $pod -n $namespace
+done
