4516f44413576aea1d99faaa7d82e307476c6a09
Stephanie Tavares <78547969+stephanie-tavares@users.noreply.github.com>
2023-03-15 15:55:06 -0300

    Update clone-projects.sh

--- clone-projects.sh
+++ clone-projects.sh
@@ -2,9 +2,9 @@
 value=cat list.txt
 for i in $value
 do
-    kubectx BaaS-Polus-prod
+    kubectx <namespace-prod>
     kubectl get hpa -n $i -o yaml > $i-update.yaml
-    kubectx BaaS-Polus-hom
+    kubectx <namespace-homolog>
     kubectl -n argocd patch --type='merge' application $i-hom -p "{\"spec\":{\"syncPolicy\":null}}"
     gsed -i 's/prod/hom/g' $i-update.yaml
      kubectl -n $i replace -f $i-update.yaml --force
